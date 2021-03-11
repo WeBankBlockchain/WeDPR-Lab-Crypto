7eb9dc92ede5e29359726aafcd250790c22f1787
cyjseagull <yujiechen_hust@163.com>
2021-03-11 18:57:56 +0800

    Refactor ffi_c_crypto, return the structure, and add a memory release interface

--- ffi/ffi_c/ffi_c_crypto/include/WeDPRCrypto.h
+++ ffi/ffi_c/ffi_c_crypto/include/WeDPRCrypto.h
@@ -1,191 +1,165 @@
 #ifndef _WEDPR_CRYPTO_H_
 #define _WEDPR_CRYPTO_H_
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <new>
+#include <ostream>
 
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdlib.h>
+struct HashResult
+{
+    char* data;
+    uintptr_t len;
+};
+
+struct KeyPairData
+{
+    char* public_key;
+    uintptr_t public_key_len;
+    char* private_key;
+    uintptr_t private_key_len;
+};
+
+struct PublicKey
+{
+    char* public_key;
+    uintptr_t public_key_len;
+};
+
+struct SignatureResult
+{
+    char* signature_data;
+    uintptr_t signature_len;
+};
 
 extern "C" {
-/**
- * C interface for 'wedpr_keccak256_hash_binary'.
- */
-char* wedpr_keccak256_hash_binary(const char* encoded_message, uintptr_t message_len);
 
-/**
- * C interface for 'wedpr_sm3_hash_binary'.
- */
-char* wedpr_sm3_hash_binary(const char* encoded_message, uintptr_t message_len);
+/// C interface for 'wedpr_keccak256_hash_binary'.
+HashResult wedpr_keccak256_hash_binary(const char* encoded_message, uintptr_t message_len);
 
-/**
- * C interface for 'wedpr_secp256k1_gen_binary_key_pair'.
- */
-char* wedpr_secp256k1_gen_binary_key_pair(void);
+/// C interface for 'wedpr_sm3_hash_binary'.
+HashResult wedpr_sm3_hash_binary(const char* encoded_message, uintptr_t message_len);
 
-/**
- * C interface for 'wedpr_secp256k1_derive_binary_public_key'.
- */
-char* wedpr_secp256k1_derive_binary_public_key(
+void dealloc_hash_result(HashResult hash_result);
+
+/// C interface for 'wedpr_secp256k1_gen_binary_key_pair'.
+KeyPairData wedpr_secp256k1_gen_binary_key_pair();
+
+/// C interface for 'wedpr_secp256k1_derive_binary_public_key'.
+PublicKey wedpr_secp256k1_derive_binary_public_key(
     const char* encoded_private_key, uintptr_t encoded_private_key_len);
 
-/**
- * C interface for 'wedpr_secp256k1_sign_binary'.
- */
-char* wedpr_secp256k1_sign_binary(const char* encoded_private_key,
+/// C interface for 'wedpr_secp256k1_sign_binary'.
+SignatureResult wedpr_secp256k1_sign_binary(const char* encoded_private_key,
     uintptr_t encoded_private_key_len, const char* encoded_message_hash,
     uintptr_t encoded_message_hash_len);
 
-/**
- * C interface for 'wedpr_secp256k1_verify_binary'.
- */
+/// C interface for 'wedpr_secp256k1_verify_binary'.
 int8_t wedpr_secp256k1_verify_binary(const char* encoded_public_key,
     uintptr_t encoded_public_key_len, const char* encoded_message_hash,
     uintptr_t encoded_message_hash_len, const char* encoded_signature,
     uintptr_t encoded_signature_len);
 
-/**
- * C interface for 'wedpr_secp256k1_recover_binary_public_key'.
- */
-char* wedpr_secp256k1_recover_binary_public_key(const char* encoded_message_hash,
+/// C interface for 'wedpr_secp256k1_recover_binary_public_key'.
+PublicKey wedpr_secp256k1_recover_binary_public_key(const char* encoded_message_hash,
     uintptr_t encoded_message_hash_len, const char* encoded_signature,
     uintptr_t encoded_signature_len);
 
-/**
- * C interface for 'wedpr_sm2_gen_binary_key_pair'.
- */
-char* wedpr_sm2_gen_binary_key_pair(void);
+/// C interface for 'wedpr_sm2_gen_binary_key_pair'.
+KeyPairData wedpr_sm2_gen_binary_key_pair();
 
-/**
- * C interface for 'wedpr_sm2_derive_binary_public_key'.
- */
-char* wedpr_sm2_derive_binary_public_key(
+/// C interface for 'wedpr_sm2_derive_binary_public_key'.
+PublicKey wedpr_sm2_derive_binary_public_key(
     const char* encoded_private_key, uintptr_t encoded_private_key_len);
 
-/**
- * C interface for 'wedpr_sm2_sign_binary'.
- */
-char* wedpr_sm2_sign_binary(const char* encoded_private_key, uintptr_t encoded_private_key_len,
-    const char* encoded_message_hash, uintptr_t encoded_message_hash_len);
+/// C interface for 'wedpr_sm2_sign_binary'.
+SignatureResult wedpr_sm2_sign_binary(const char* encoded_private_key,
+    uintptr_t encoded_private_key_len, const char* encoded_message_hash,
+    uintptr_t encoded_message_hash_len);
 
-/**
- * C interface for 'wedpr_sm2_sign_binary_fast'.
- */
-char* wedpr_sm2_sign_binary_fast(const char* encoded_private_key, uintptr_t encoded_private_key_len,
-    const char* encoded_public_key, uintptr_t encoded_public_key_len,
-    const char* encoded_message_hash, uintptr_t encoded_message_hash_len);
+/// C interface for 'wedpr_sm2_sign_binary_fast'.
+SignatureResult wedpr_sm2_sign_binary_fast(const char* encoded_private_key,
+    uintptr_t encoded_private_key_len, const char* encoded_public_key,
+    uintptr_t encoded_public_key_len, const char* encoded_message_hash,
+    uintptr_t encoded_message_hash_len);
 
-/**
- * C interface for 'wedpr_sm2_verify_binary'.
- */
+/// C interface for 'wedpr_sm2_verify_binary'.
 int8_t wedpr_sm2_verify_binary(const char* encoded_public_key, uintptr_t encoded_public_key_len,
     const char* encoded_message_hash, uintptr_t encoded_message_hash_len,
     const char* encoded_signature, uintptr_t signature_len);
 
-/**
- * C interface for 'wedpr_secp256k1_ecies_encrypt'.
- */
+void dealloc_key_pair(KeyPairData key_pair);
+
+void dealloc_signature_data(SignatureResult signature_result);
+
+void dealloc_public_key_data(PublicKey public_key_result);
+
+/// C interface for 'wedpr_secp256k1_ecies_encrypt'.
 char* wedpr_secp256k1_ecies_encrypt(char* encoded_public_key, char* encoded_plaintext);
 
-/**
- * C interface for 'wedpr_secp256k1_ecies_decrypt'.
- */
+/// C interface for 'wedpr_secp256k1_ecies_decrypt'.
 char* wedpr_secp256k1_ecies_decrypt(char* encoded_private_key, char* encoded_ciphertext);
 
-/**
- * C interface for 'wedpr_keccak256_hash'.
- */
+/// C interface for 'wedpr_keccak256_hash'.
 char* wedpr_keccak256_hash(const char* encoded_message);
 
-/**
- * C interface for 'wedpr_sm3_hash'.
- */
+/// C interface for 'wedpr_sm3_hash'.
 char* wedpr_sm3_hash(const char* encoded_message);
 
-/**
- * C interface for 'wedpr_secp256k1_gen_key_pair'.
- */
-char* wedpr_secp256k1_gen_key_pair(void);
+/// C interface for 'wedpr_secp256k1_gen_key_pair'.
+char* wedpr_secp256k1_gen_key_pair();
 
-/**
- * C interface for 'wedpr_secp256k1_derive_public_key'.
- */
+/// C interface for 'wedpr_secp256k1_derive_public_key'.
 char* wedpr_secp256k1_derive_public_key(const char* encoded_private_key);
 
-/**
- * C interface for 'wedpr_secp256k1_sign'.
- */
+/// C interface for 'wedpr_secp256k1_sign'.
 char* wedpr_secp256k1_sign(const char* encoded_private_key, const char* encoded_message_hash);
 
-/**
- * C interface for 'wedpr_secp256k1_verify'.
- */
+/// C interface for 'wedpr_secp256k1_verify'.
 int8_t wedpr_secp256k1_verify(const char* encoded_public_key, const char* encoded_message_hash,
     const char* encoded_signature);
 
-/**
- * C interface for 'wedpr_secp256k1_recover_public_key'.
- */
+/// C interface for 'wedpr_secp256k1_recover_public_key'.
 char* wedpr_secp256k1_recover_public_key(
     const char* encoded_message_hash, const char* encoded_signature);
 
-/**
- * C interface for 'wedpr_sm2_gen_key_pair'.
- */
-char* wedpr_sm2_gen_key_pair(void);
+/// C interface for 'wedpr_sm2_gen_key_pair'.
+char* wedpr_sm2_gen_key_pair();
 
-/**
- * C interface for 'wedpr_sm2_derive_public_key'.
- */
+/// C interface for 'wedpr_sm2_derive_public_key'.
 char* wedpr_sm2_derive_public_key(const char* encoded_private_key);
 
-/**
- * C interface for 'wedpr_sm2_sign'.
- */
+/// C interface for 'wedpr_sm2_sign'.
 char* wedpr_sm2_sign(const char* encoded_private_key, const char* encoded_message_hash);
 
-/**
- * C interface for 'wedpr_sm2_sign_fast'.
- */
+/// C interface for 'wedpr_sm2_sign_fast'.
 char* wedpr_sm2_sign_fast(const char* encoded_private_key, const char* encoded_public_key,
     const char* encoded_message_hash);
 
-/**
- * C interface for 'wedpr_sm2_verify'.
- */
+/// C interface for 'wedpr_sm2_verify'.
 int8_t wedpr_sm2_verify(const char* encoded_public_key, const char* encoded_message_hash,
     const char* encoded_signature);
 
-/**
- * C interface for 'wedpr_curve25519_vrf_derive_public_key'.
- */
+/// C interface for 'wedpr_curve25519_vrf_derive_public_key'.
 char* wedpr_curve25519_vrf_derive_public_key(const char* encoded_private_key);
 
-/**
- * C interface for 'wedpr_curve25519_vrf_prove_utf8'.
- */
+/// C interface for 'wedpr_curve25519_vrf_prove_utf8'.
 char* wedpr_curve25519_vrf_prove_utf8(const char* encoded_private_key, const char* utf8_message);
 
-/**
- * C interface for 'wedpr_curve25519_vrf_prove_fast_utf8'.
- */
+/// C interface for 'wedpr_curve25519_vrf_prove_fast_utf8'.
 char* wedpr_curve25519_vrf_prove_fast_utf8(
     const char* encoded_private_key, const char* encoded_public_key, const char* utf8_message);
 
-/**
- * C interface for 'wedpr_curve25519_vrf_verify_utf8'.
- */
+/// C interface for 'wedpr_curve25519_vrf_verify_utf8'.
 int8_t wedpr_curve25519_vrf_verify_utf8(
     const char* encoded_public_key, const char* utf8_message, const char* encoded_proof);
 
-/**
- * C interface for 'wedpr_curve25519_vrf_proof_to_hash'.
- */
+/// C interface for 'wedpr_curve25519_vrf_proof_to_hash'.
 char* wedpr_curve25519_vrf_proof_to_hash(const char* encoded_proof);
 
-/**
- * C interface for 'wedpr_curve25519_vrf_is_valid_public_key'.
- */
+/// C interface for 'wedpr_curve25519_vrf_is_valid_public_key'.
 int8_t wedpr_curve25519_vrf_is_valid_public_key(const char* encoded_public_key);
-}
+
+}  // extern "C"
 
 #endif
--- ffi/ffi_c/ffi_c_crypto/src/binary_hash.rs
+++ ffi/ffi_c/ffi_c_crypto/src/binary_hash.rs
@@ -13,6 +13,14 @@ use crate::config::HASH_KECCAK256;
 use crate::config::HASH_SM3;
 
 use libc::c_char;
+
+// Rust to c/c++
+#[repr(C)]
+pub struct HashResult {
+    data: *mut c_char,
+    len: usize,
+}
+
 // Keccak256 implementation.
 #[cfg(feature = "wedpr_f_hash_keccak256")]
 #[no_mangle]
@@ -20,7 +28,7 @@ use libc::c_char;
 pub extern "C" fn wedpr_keccak256_hash_binary(
     encoded_message: *const c_char,
     message_len: usize,
-) -> *mut c_char
+) -> HashResult
 {
     unsafe {
         // Note: Since encode_message is an object passed in by C/CPP, it should
@@ -30,10 +38,15 @@ pub extern "C" fn wedpr_keccak256_hash_binary(
             message_len,
             message_len,
         );
-        let hash_result = HASH_KECCAK256.hash(&input_message);
+        let mut hash_result = HASH_KECCAK256.hash(&input_message);
         std::mem::forget(input_message);
-        // The ownership of hash_result has not been changed
-        return hash_result.as_ptr() as *mut c_char;
+        let hash_ptr = hash_result.as_mut_ptr();
+        let length = hash_result.len();
+        std::mem::forget(hash_result);
+        HashResult {
+            data: hash_ptr as *mut c_char,
+            len: length,
+        }
     }
 }
 
@@ -44,7 +57,7 @@ pub extern "C" fn wedpr_keccak256_hash_binary(
 pub extern "C" fn wedpr_sm3_hash_binary(
     encoded_message: *const c_char,
     message_len: usize,
-) -> *mut c_char
+) -> HashResult
 {
     unsafe {
         let input_message = Vec::from_raw_parts(
@@ -52,8 +65,28 @@ pub extern "C" fn wedpr_sm3_hash_binary(
             message_len,
             message_len,
         );
-        let hash_result = HASH_SM3.hash(&input_message);
+        let mut hash_result = HASH_SM3.hash(&input_message);
         std::mem::forget(input_message);
-        return hash_result.as_ptr() as *mut c_char;
+        let hash_ptr = hash_result.as_mut_ptr();
+        let length = hash_result.len();
+        std::mem::forget(hash_result);
+        HashResult {
+            data: hash_ptr as *mut c_char,
+            len: length,
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn dealloc_hash_result(hash_result: HashResult) {
+    unsafe {
+        if hash_result.data.is_null() {
+            return;
+        }
+        Vec::from_raw_parts(
+            hash_result.data as *mut i8,
+            hash_result.len,
+            hash_result.len,
+        );
     }
 }
