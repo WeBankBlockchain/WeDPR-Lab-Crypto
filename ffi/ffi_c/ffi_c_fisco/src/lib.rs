d6fb2bd642e629ee75352e51073c169b99746f2c
HaoXuan40404 <444649358@qq.com>
2021-03-23 19:19:26 +0800

    add fisco bcos precompile bn128

---
+++ ffi/ffi_c/ffi_c_fisco/src/lib.rs
@@ -0,0 +1,6 @@
+// Copyright 2020 WeDPR Lab Project Authors. Licensed under Apache-2.0.
+
+//! Library of FFI of fisco bcos wrapper functions, targeting C/C++
+//! compatible architectures (including iOS).
+
+pub mod bn128;
