dad9db9ef87c9dbb1359b4f05522891953c9a2e8
shareong <740310627@qq.com>
2023-09-20 16:01:43 +0800

    optimize code structure

--- crypto/ecc/ffi_java_ecc_ed25519/src/lib.rs
+++ crypto/ecc/ffi_java_ecc_ed25519/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate jni;
 
-use psi_utils::{
-    hash_to_curve, point_scalar_multi, random_scalar, scalar_inverse,
+use ecc_ed25519::{
+    hash_to_curve, point_scalar_multi, random_scalar,
 };
 
 use jni::{objects::JClass, sys::jbyteArray, JNIEnv};
@@ -37,28 +37,6 @@ pub extern "system" fn Java_com_webank_wedpr_crypto_NativeInterface_hashToCurve(
 
     // 调用原始函数
     let result = hash_to_curve(&message_bytes);
-
-    // 将 Vec<u8> 转换成 jbyteArray 并返回给Java层
-    match env.byte_array_from_slice(&result) {
-        Ok(array) => array,
-        Err(_) => env.new_byte_array(0).unwrap(), // 返回空的 jbyteArray
-    }
-}
-
-#[no_mangle]
-pub extern "system" fn Java_com_webank_wedpr_crypto_NativeInterface_scalarInverse(
-    env: JNIEnv,
-    _class: JClass,
-    scalar: jbyteArray,
-) -> jbyteArray {
-    // 将 jbyteArray 转换成 Vec<u8>
-    let scalar_bytes = match env.convert_byte_array(scalar) {
-        Ok(bytes) => bytes,
-        Err(_) => return env.new_byte_array(0).unwrap(), /* 返回空的 jbyteArray */
-    };
-
-    // 调用原始函数
-    let result = scalar_inverse(&scalar_bytes);
 
     // 将 Vec<u8> 转换成 jbyteArray 并返回给Java层
     match env.byte_array_from_slice(&result) {
--- crypto/ecc/ffi_wasm_ecc_ed25519/src/lib.rs
+++ crypto/ecc/ffi_wasm_ecc_ed25519/src/lib.rs
@@ -1,14 +1,9 @@
 extern crate wasm_bindgen;
 
-use psi_utils::{
-    hash_to_curve, point_scalar_multi, random_scalar, scalar_inverse,
+use ecc_ed25519::{
+    hash_to_curve, point_scalar_multi, random_scalar,
 };
 use wasm_bindgen::prelude::wasm_bindgen;
-
-#[wasm_bindgen]
-pub fn wasm_scalar_inverse(scalar: &[u8]) -> Vec<u8> {
-    scalar_inverse(scalar)
-}
 
 #[wasm_bindgen]
 pub fn wasm_point_scalar_multi(point: &[u8], scalar: &[u8]) -> Vec<u8> {
